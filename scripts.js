3bfceb5157daec6e1c32085cf78b7855a116ef4d
alimakouar <makouarali@gmail.com>
2022-03-11 13:21:01 +0100

    drawing the game area

--- scripts.js
+++ scripts.js
@@ -1,2 +1,6 @@
 const can=document.getElementById("snakeGame");
 const can_ctxt=snakeGame.getContext("2d");
+
+can_ctxt.fillStyle="black"
+can_ctxt.fillRect(0,0,can.width,can.height)
+can.style.border="2px solid red";

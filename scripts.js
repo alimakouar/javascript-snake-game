f2d5f572e6e882645267096ec28f6bb81dfc61b2
alimakouar <makouarali@gmail.com>
2022-03-11 13:21:56 +0100

    draw the snake

--- scripts.js
+++ scripts.js
@@ -4,3 +4,22 @@ const can_ctxt=snakeGame.getContext("2d");
 can_ctxt.fillStyle="black"
 can_ctxt.fillRect(0,0,can.width,can.height)
 can.style.border="2px solid red";
+
+let snake=[{x:400,y:300},{x:389,y:300},{x:378,y:300},{x:367,y:300},{x:356,y:300},{x:345,y:300}];
+
+function drawSquare(square)
+{
+    can_ctxt.fillStyle='red';
+    can_ctxt.strokestyle='white';
+    can_ctxt.fillRect(square.x,square.y,10,10);
+}
+
+function drawSnake(snake){
+    snake.forEach(drawSquare);
+}
+
+//fonction main qui dessine le serpent la 1ere fois
+function main(){
+    var snake=[{x:400,y:300},{x:389,y:300},{x:378,y:300},{x:367,y:300},{x:356,y:300},{x:345,y:300}];
+    drawSnake(snake);
+}
